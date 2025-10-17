5d02dfbe405af338177806c2c942ac87f0cfea65
Ian Utbjoa <ian.utbjoa@knowit.no>
2025-10-17 09:49:34 +0200

    backend first

---
+++ backend/OnlyPikks/src/test/java/com/equinor/onlypikks/OnlyPikksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equinor.onlypikks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyPikksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
